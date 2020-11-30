a1663695240f3a9c1eb47c501f0ee0bb68efb4f3
arielnw <winataariel@gmail.com>
2020-11-30 23:45:31 +0700

    Add files via upload

---
+++ service.js
@@ -0,0 +1,61 @@
+// urlB64ToUint8Array adalah fungsi untuk mengkodedata ke base64 public key
+// untuk buffer Array yang dibutuhkan untuk opsi subscription
+const urlB64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+    const rawData = atob(base64)
+    const outputArray = new Uint8Array(rawData.length)
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray
+  }
+
+  // fungsi saveSubscription akan menyimpan subscription pada backend
+const saveSubscription = async subscription => {
+    const SERVER_URL = 'http://localhost:8080/save-subscription'
+    const response = await fetch(SERVER_URL, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(subscription),
+    })
+    return response.json()
+  }
+
+  self.addEventListener('activate', async () => {
+    // Fungsi ini dipanggil jika services worker aktif
+    console.log('activated')
+    try {
+      const applicationServerKey = urlB64ToUint8Array(
+        'BIpYrlIYvqrtQgsdywWpRgmDqrCI21xbP4Tgxi543MlCTfAyBAyTO0ToDhDPJbETqtl3Rrnp2I5sBpnCi_xp2SM'
+      )
+      const options = { applicationServerKey, userVisibleOnly: true }
+      const subscription = await self.registration.pushManager.subscribe(options)
+      console.log('subscription', subscription)
+        const response = await saveSubscription(subscription)
+        console.log(response)
+    } catch (err) {
+        console.log('Error', err)
+    }
+  })
+
+  //fungsi untuk push data dari server ke client side
+  self.addEventListener('push', function(event) {
+    if (event.data) {
+      console.log("New Job Added! ", event.data.text());
+      showLocalNotification("New Job Added!", event.data.text(),  self.registration);
+    } else {
+      console.log("Push event but no data");
+    }
+  });
+
+  //untuk membuat interface localNotification
+  const showLocalNotification = (title, body, swRegistration) => {
+    const options = {
+      body 
+      // disini dapat menambahkan properties seperti icon, image, vibrate, dll.
+    };
+    swRegistration.showNotification(title, options);
+  };
---
+++ sub.js
@@ -0,0 +1,56 @@
+//fungsi untuk mengecek apakah browser yang dipakai support service worker
+const check = () => {
+  if (!('serviceWorker' in navigator)) {
+    throw new Error('No Service Worker support!')
+  }
+  if (!('PushManager' in window)) {
+    throw new Error('No Push API Support!')
+  }
+}
+
+//fungsi untuk register service worker dengan menggunakan fungsi di file service.js
+const registerServiceWorker = async () => {
+  const swRegistration = await navigator.serviceWorker.register('service.js')
+  return swRegistration
+}
+
+//fungsi untuk memunculkan notification permission
+const requestNotificationPermission = async () => {
+  const permission = await window.Notification.requestPermission()
+  // permission dapat dibagi menjadi tiga, yaitu 'granted', 'default', 'denied'
+  // granted: user menyetujui request
+  // default: user menutup jendela notification permission dengan mengklik x (close) 
+  // denied: user menolak request
+  if (permission !== 'granted') {
+    throw new Error('Permission not granted for Notification')
+  }
+}
+
+//fungsi untuk memunculkan notifikasi dan subscribe
+const main = async () => {
+  check()
+  const swRegistration = await registerServiceWorker()
+  const permission = await requestNotificationPermission()
+  subscribe()
+}
+
+//fungsi untuk mengambil data channel pada halaman subscribe dan menampilkan data job list 
+var messageAja = []; //array untuk menampung message dari publisher
+function subscribe() {
+  var message = document.getElementById('message');
+  var channel = document.getElementById('channel').value;
+  var host = window.document.location.host.replace(/:.*/, '');
+  var ws = new WebSocket('ws://' + host + ':8080');
+  ws.onopen = function () {
+      ws.send(JSON.stringify({
+          request: 'SUBSCRIBE',
+          message: '',
+          channel: channel
+      }));
+      ws.onmessage = function(event){
+          data = JSON.parse(event.data); 
+          messageAja.push(data.message); //push ke array
+          message.innerHTML = messageAja; //display ke array
+      };
+  };  
+}
